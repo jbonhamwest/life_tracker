d247678bf791003a5ba7ca78e3925a9f50482a84
jbonhamwest <jbwest@nc.rr.com>
2014-06-18 21:14:00 -0400

    Validated 2 columns of school dates

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ActiveRecord::Base
+  validates :year_started, presence: true, numericality:
+      { only_integer: true,
+        less_than_or_equal_to: :year_ended }
+  validates :year_ended, presence: true, numericality: { only_integer: true }
+  validates :school_name, presence: true
 end
